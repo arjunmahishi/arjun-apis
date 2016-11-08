163a2490af0e6ba76c9a1c9f8951e9a6fe0eebbf
= <arjun.mahishi@gmail.com>
2016-11-08 07:53:34 +0530

    added bootstrap to twitter api page

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ from twitter import Twitter
 
 app = Flask(__name__)
 
+def filterText(text):
+	clean_text = ""
+	allowed = """qwertyuiopasdfghjklzxcvbnm ,.-/\\\"\'?
+	QWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-+=_`~<>;:|[]}{"""
+	for letter in text:
+		if letter in allowed:
+			clean_text += letter
+	return clean_text
+
 @app.route('/guitar-tabs/', methods=['GET'])
 def display():
 	query = request.args.get('query') or None
 	if query:
 		try:
 			data = ultimate.getContents(ultimate.getBestLink(query))
-			tab = data.replace("\n", " <br> ")
-			html_code = open('templates/tabs.html').read()
+			button = '''<a href="/guitar-tabs/"><button class="btn btn-primary">GO BACK</button></a><br><br>'''
+			tab = button + data.replace("\n", " <br> ")
+			html_code = open('templates/frame.html').read()
 			file = open('templates/tabs1.html', 'w')
 			file.write(html_code.replace("~~~CUE~~~", tab))
 			file.close()
@@ -31,12 +41,16 @@ def twitter():
 			tweets = tObj.getTweets(tag)
 			display = ""
 			for tweet in tweets:
-				display += tweet.text + "<br>"
-			return display
+				display += filterText(tweet.text) + "<br><br>"
+			html_code = open('templates/frame.html').read()
+			file = open('templates/tweets.html', 'w')
+			file.write(html_code.replace("~~~CUE~~~", display))
+			file.close()
+			return render_template('tweets.html')
 		except Exception as e:
 			return "<br><br><p class='lead'>ERROR 111</p><br><br>Something went wrong.." + str(e)
 	else:
-		return "Test" 		
+		return render_template("twitter_home.html")		
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=8000)
